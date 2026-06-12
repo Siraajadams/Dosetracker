046c366f03e3b6e014401748e04d1d7c123ff60a
Siraajadams <siraaj.adams2003@gmail.com>
2026-06-12 19:55:20 +0200

    Update page.tsx

--- app/dosetrack/page.tsx
+++ app/dosetrack/page.tsx
@@ -1,1 +1,8 @@
-
+export default function DoseTrackPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>DoseTrack Calendar</h1>
+      <p>This dashboard page is working.</p>
+    </main>
+  );
+}
